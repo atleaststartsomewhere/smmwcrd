08107e35debf0c1f5430a582fe55b2a233e3764d
Ryan Koon <ryan@ryankoon.com>
2016-08-28 15:45:09 -0700

    dashboard.php - Created the start to the proper dashboard page - Calendar and notices are hybrid real-pseudo code for their final forms -- They need to be updated with the proper db linking and helper functions - I should comb through the entire file while it's this small to update it
    Signed-off-by: Ryan Koon <ryan@ryankoon.com>

--- application/controllers/admin/dashboard.php
+++ application/controllers/admin/dashboard.php
@@ -1,6 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 class Dashboard extends EXT_AdminController {
+protected $attention_widgets;
+protected $widgets;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 function __construct( ) {
 	$title = "Admin: San Miguelito Mutual Water Company";
@@ -12,26 +14,115 @@ function __construct( ) {
 
 	parent::__construct($title, $heading, $description, $page, $help, $scripts);
 	$CI =& get_instance();
+
+	$attention_widgets = array();
+	$widgets = array();
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 public function index() {
-	// Run variable assignment here
+	// Order of these function calls winds up being a display prioritization on the render side
+	// Each of these functions add themselves to the class object's $attention_widgets or $widgets array
+	//  so there's no need to do any further assignment
+	$this->make_widget_calendar();
+	$this->make_widget_notices();
 
-	// Set up the variables into array keys for the view to use
-	//$this->add_page_data(array());
+	$this->add_page_data(array(
+		'user_widget' => $this->make_widget_user(),
+		'attention_widgets' => $attention_widgets,
+		'widgets' => $widgets
+	));
 	$this->render_page();
 }
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Widget Render Functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
+private function make_widget_user() {
+	$widget_path = 'admin/widgets/dashboard-user';
 
+	$last_login = // function call - either to auth model or user object or helper function
+	$pwd_age = // function call - either to auth model or user object or helper function
+
+	$widget_data = array(
+		'billpay' => $this->billpay_available,
+		'last_login' => $last_login,
+		'pwd_age' => $pwd_age
+	);
+
+	return $this->load->view($widget_path, $widget_data, TRUE);
+}
+private function make_widget_calendar() {
+	$widget_path = 'admin/widgets/dashboard-calendar';
+	$attention = FALSE;
+	$attention_messages = array();
+
+	// Widget Data Setup
+	$current_month = $this->Calendar->get_current_month_items();
+	if ( !$current_month->success ) {
+		array_push($attention_messages, 'There are no calendar events for the current month!');
+	}
+	$next_month = $this->Calendar->get_month_items(date('Y'), date('m', strtotime('+1 month')));
+	if ( !$next_month->success ) {
+		if ( date('d') >= //last day of month - 14 ) {
+			array_push($attention_messages, 'There are no calendar events for next month and you are less than two weeks from '.date('F', strtotime('+1 month')));
+		}
+	}
+
+	$attention = (count($attention_messages) > 0) ? TRUE : FALSE;
+	$widget_data = array(
+		'current' => $current_month,
+		'next' => $next_month,
+		'attention' => $attention,
+		'attention_messages' => $attention_messages
+	);
+
+	$widget = $this->load->view($widget_path, $widget_data, TRUE);
+
+	// Test if the widget is grouped with the attention widgets or if it will go in as normal
+	if ( $attention )
+		array_push($this->attention_widgets, $widget);
+	else
+		array_push($this->widgets, $widget);
+}
+private function make_widget_notices() {
+	$widget_path = 'admin/widgets/dashboard-notices';
+	$attention = FALSE;
+	$attention_messages = array();
+
+	// Widget Data Setup
+	$notices = $this->Notices->get_notices();
+	$oldest = $this->Notices->get_oldest_date();
+	if ( $oldest < date('Y-m-d', strtotime('-1 month')) ) {
+		array_push($attention_messages, 'One or more notices are more than one month old.');
+	}
+
+	$attention = (count($attention_messages) > 0) ? TRUE : FALSE;
+	$widget_data = array(
+		'notices' => $notices,
+		'attention' => $attention,
+		'attention_messages' => $attention_messages
+	);
+
+	$widget = $this->load->view($widget_path, $widget_data, TRUE);
+
+	// Test if the widget is grouped with the attention widgets or if it will go in as normal
+	if ( $attention )
+		array_push($this->attention_widgets, $widget);
+	else
+		array_push($this->widgets, $widget);
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Helper Functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-protected function get_data() {
-	return 'someData';
+private function billpay_available() {
+	$host = $this->Content->get_content_by_key('billpay_link');
+
+	if($socket =@ fsockopen($host, 80, $errno, $errstr, 30)) {
+		return true;
+	fclose($socket);
+	} else {
+		return false;
+	}
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 } // END OF CLASS
